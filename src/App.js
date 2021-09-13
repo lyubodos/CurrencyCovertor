67d75ca661b4b131c3874e5e45e617e5ac8dd926
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-09-13 22:03:13 +0300

    Convertor finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 
 import { useEffect, useState } from 'react';
 
-
-
 import CurrencyRow from "./CurrentRow";
 
 const envVals = {
@@ -18,30 +16,66 @@ function App() {
   const [currOptions, setCurrOptions] = useState([]);
   const [fromCurr, setFromCurr] = useState();
   const [toCurr, setToCurr] = useState();
+  const [exRate, setExRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountFromCurr, setAmmountFromCurr] = useState(true);
 
+  console.log(exRate);
 
-  console.log(currOptions);
+  let toAmount, fromAmount;
+
+  if (amountFromCurr) {
+    fromAmount = amount;
+    toAmount = amount * exRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exRate;
+  };
 
   useEffect( () => {
-    
+
     fetch(BASE_URL)
-    .then(res => res.json())
-    .then(data => {
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const firstCurrency = Object.keys(data.rates)[0];
 
-    setFromCurr(data.base)
-    setCurrOptions([data.base, ...Object.keys(data.rates)])  
-    
-  })
+        setFromCurr(data.base);
+        setCurrOptions([data.base, ...Object.keys(data.rates)]);
+        setToCurr(firstCurrency);
+        setExRate(data.rates[firstCurrency])
+      })
 
-  }, [])
+  }, []);
 
 
+  useEffect(() => {
+
+    if(fromCurr !== null && toCurr !== null){
+      fetch(`${BASE_URL}?base=${fromCurr}&symbols=${toCurr}`)
+      .then(res => res.json())
+      .then(data => setExRate(data.rates[toCurr]))
+
+    }
+ 
+  } ,[fromCurr, toCurr]);
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmmountFromCurr(false);
+  }
+
+
+  function handleAmountChange(e) {
+    setAmount(e.target.value);
+    setAmmountFromCurr(true);
+  }
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow currOptions={currOptions} />
+      <CurrencyRow currOptions={currOptions} onChangeCurrency={e => setFromCurr(e.target.value)} selectedCurr={fromCurr}  amount ={fromAmount} onChangeAmount={handleAmountChange}/>
       <div className="equals">=</div>
-      <CurrencyRow  currOptions={currOptions} />
+      <CurrencyRow currOptions={currOptions} onChangeCurrency={e => setToCurr(e.target.value)} selectedCurr={toCurr} amount ={toAmount} onChangeAmount={handleToAmountChange}/>
     </>
   );
 }
--- src/CurrentRow.js
+++ src/CurrentRow.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-export default function CurrentRow({ currOptions }) {
+export default function CurrentRow(
+    {   
+        amount,
+        currOptions,
+        selectedCurr,
+        onChangeCurrency,
+        onChangeAmount
+    }
+) {
 
     return (
         <div>
-            <input type="number" className="inout" />
-            <select>
+            <input type="number" className="inout" value={amount} onChange={onChangeAmount} />
+            <select value={selectedCurr} onChange={onChangeCurrency}>
                 {currOptions.map(option => {
                     return <option key={option} value={option}>
                         {option}
